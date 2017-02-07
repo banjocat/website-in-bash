ad82c214464dc8fbff45bb5ee2eed39d0b0beb89
banjocat <jackmuratore@gmail.com>
2017-02-07 01:07:12 -0500

    Multiline echos

--- create_routes.sh
+++ create_routes.sh
@@ -6,15 +6,17 @@ files=$(find $HTML_PATH -type f)
 cat route.header
 for i in $files; do
     route=$(echo $i | sed "s/$HTML_PATH\///")
-    echo "    \"/$route\")"
-    echo "        cat $i"
-    echo "    ;;"
+    echo "
+    \"/$route\")
+        cat $i
+    ;;"
 done
 
-echo "    *)"
-echo "        cat ./404.html"
-echo "    ;;"
-echo esac
+echo "
+    *)
+        cat ./404.html
+    ;;
+esac"
 
 
 
